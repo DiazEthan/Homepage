c1dfa2372417a80756cab57db7714d61c77e571f
DiazEthan <ediaz021502@gmail.com>
2024-01-13 15:03:35 -0600

    Update JS

---
+++ js.js
@@ -0,0 +1,18 @@
+//Ethan Diaz
+//1/13/24
+
+var x = 7; 
+
+x = 7;
+
+var y = 3;
+
+y = 3;
+
+total = x + y;
+
+console.log("7 + 3 = " + total);
+
+var movie = prompt("What is your favorite movie?");
+
+console.log("The user favorite movie is: \n \n" + movie);
