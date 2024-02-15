bdef32d2b8d1b4aef7268c36524702d600de93ad
DiazEthan <ediaz021502@gmail.com>
2024-02-15 17:12:13 -0600

    Unit_1_Project

---
+++ unit_1_project.js
@@ -0,0 +1,14 @@
+
+if ( number = 1 ) {
+    var htmlColor = document.getElementById("html");
+	var divColor = document.getElementById("div");
+    htmlColor.style.backgroundColor = "#ff000";
+} else if ( number = 2 ) {
+    var htmlColor = document.getElementById("html");
+	var divColor = document.getElementById("div");
+    htmlColor.style.backgroundColor = "#ffa500";
+} else if ( number = 3 ) {
+    var htmlColor = document.getElementById("html");
+	var divColor = document.getElementById("div");
+    htmlColor.style.backgroundColor = "#00ff00";
+}
